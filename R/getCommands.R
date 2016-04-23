084aba68f73980342cd3a335da9f1fc153db95dc
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-04-23 21:43:50 +0200

    extract commands

---
+++ R/getCommands.R
@@ -0,0 +1,25 @@
+#' Read and Decode Commands
+#'
+#' Read file with commands from a directory and use the 'decode' function to decode it.
+#'
+#' @param fname Name of a file to read and transform.
+#'
+#' @author
+#' Przemyslaw Biecek, \email{przemyslaw.biecek@@gmail.com}
+#'
+#' @export
+getCommands <- function(fname) {
+  lines <- readLines(fname)
+  tlines <- grep(lines, pattern="## TASK")
+  tstart <- tlines + 2
+  tend <- c(tlines[-1]-1, length(tlines))
+
+  # tasks ids
+  tnames <- gsub(lines[tlines], pattern="## TASK: +", replacement="")
+  tcont <- sapply(seq_along(tlines), function(i){
+    paste(lines[tstart[i]:tend[i]], collapse="\n")
+  })
+  tcont <- dcode(tcont)
+  names(tcont) <- tnames
+  tcont
+}
